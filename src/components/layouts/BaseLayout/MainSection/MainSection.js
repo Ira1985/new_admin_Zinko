3fc4a36948df319d8c833082620ac252e72f345b
MamchychO <paganini.oleh@gmail.com>
2020-06-27 22:18:09 +0300

    Rebuild mainSection.

--- src/components/layouts/BaseLayout/MainSection/MainSection.js
+++ src/components/layouts/BaseLayout/MainSection/MainSection.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import {withTranslation} from "react-i18next";
 import './mainSection.scss';
+import {BreadCrumb} from "primereact/breadcrumb";
+import {Button} from "primereact/button";
+import {Toolbar} from "primereact/toolbar";
 
 class MainSection extends Component {
 
@@ -9,10 +12,20 @@ class MainSection extends Component {
     }
 
     render() {
-        const {t} = this.props;
+        const {t, breadcrumbs} = this.props;
         return <>
             <div className='main-section'>
-                <div className='header'></div>
+                <div className='header'>
+                    <Toolbar>
+                        <div className="p-toolbar-group-left">
+                            <BreadCrumb model={breadcrumbs} home={{label: 'AAA', icon: 'pi pi-home'}} />
+                        </div>
+                        <div className="p-toolbar-group-right">
+                            <Button label='Пакетная обработка' style={{marginRight:'.25em'}} />
+                            <Button label='Добавить' className="p-button-success" onClick={() => this.setState({visibleAdd: true})} />
+                        </div>
+                    </Toolbar>
+                </div>
                 <hr/>
             </div>
             </>;
