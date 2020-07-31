48bee05eaaa91a496d42292c01eae4e4f3d3a1e4
MamchychO <paganini.oleh@gmail.com>
2020-07-31 07:39:44 +0300

    Add buttons for categories.

--- src/components/Categories/CategoriesNew.js
+++ src/components/Categories/CategoriesNew.js
@@ -5,6 +5,10 @@ import CountryEditDialog from "../Countries/Edit/CountryEditDialog";
 import BaseLayout from "../layouts/BaseLayout/BaseLayout";
 import Category, {CategorySchema} from "../../models/Category";
 import {categoryNewService} from "../../service/categoryNew.service";
+import {Button} from "primereact/button";
+import {history} from "../../App";
+import SubsGroup from "../../models/SubsGroup";
+import GridColumn from "../../models/base/GridColumn";
 
 const plurals = ['models.plurals.first', 'models.plurals.second', 'models.plurals.third'];
 
@@ -19,6 +23,32 @@ class CategoriesNew extends Component {
         return (
             <CountryEditDialog loading={loading} editedItem={editItem} updateValue={updateValue} />
         );
+    }
+
+    renderActionColumns(rowData, column) {
+        const {t} = this.props;
+        return <div className={'column-button'}>
+            {/*<Button icon="pi p-empty-button chain-ico" onClick={() => {
+                history.push('/subsGroups/' + rowData.id + '/values');
+            }} tooltip={t('subsGroups.fields.showSubs')}/>*/}
+            <Button icon="pi p-empty-button case-ico"/>
+            <Button icon="pi p-empty-button plus-ico"/>
+            <Button icon="pi p-empty-button times-ico"/>
+            <Button icon="pi p-empty-button chain-ico"/>
+        </div>
+    }
+
+    buildColumns() {
+        let columns = SubsGroup.buildColumns();
+        columns.push(new GridColumn().build({field: '', header: '', style: {textAlign:'center'}, actionColumn: true,
+            order: 50,
+            actionWidth: 120,
+            default: true,
+            /*widthCoef: 1,*/
+            bodyStyle: {textAlign: 'center'},
+            renderer: (rowData, column) => this.renderActionColumns(rowData, column)}));
+
+        return columns;
     }
 
     render() {
@@ -38,7 +68,7 @@ class CategoriesNew extends Component {
                         location={this.props.location}
                         gridView={false}
                         treeView={true}
-                        columns={Category.buildColumns()}
+                        columns={this.buildColumns()}
                         editComponent={this.editComponent}
             >
             </BaseLayout>
